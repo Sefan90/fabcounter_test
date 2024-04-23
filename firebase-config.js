c17b8561bd424da8ac59cdd70525e3e4a1355541
Stefan <stevetv90@gmail.com>
2024-04-23 20:36:55 +0200

    Fork commit

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+var firebaseConfig = (function() {
+    var firebaseConfig = {
+    apiKey: "AIzaSyBdCH_iBKsjGJeoSFXuYeiqgDO6aYXhuoM",
+    authDomain: "fabcounter-2df64.firebaseapp.com",
+    databaseURL: "https://fabcounter-2df64-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "fabcounter-2df64",
+    storageBucket: "fabcounter-2df64.appspot.com",
+    messagingSenderId: "951880555106",
+    appId: "1:951880555106:web:ec17bba169cf6eb4a18f3b",
+    measurementId: "G-7E1Y8R5CFX"
+    };
+    return firebaseConfig;
+})();
